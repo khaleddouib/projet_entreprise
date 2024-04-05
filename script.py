964588275f43ab9662a48c77c00bbb706b4c0daa
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2024-04-05 18:59:12 +0200

    modification par khaled

--- script.py
+++ script.py
@@ -1,3 +1,4 @@
 mon code python
+code ajouté par moi au meme temps que la collegue
 code rajoté par moi et modifier plus tard pacequ il y avait des erreurs
 code supplémentaire ajouter par ma collegue
